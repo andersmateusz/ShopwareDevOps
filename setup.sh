d7a41e76dffb4b227e06b5a9389feae069d7f510
Mateusz Anders <Mateusz.Anders@sunzinet.com>
2023-02-17 11:27:24 +0100

    rm and stop and copy fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 PROJECT_REPO_URL="https://github.com/andersmateusz/ShopwareSampleProject.git"
+docker compose stop
+rm -dr shopware
 mkdir shopware
 mkdir shopware/html
 id=$(docker run -d dockware/dev:latest)
@@ -8,7 +10,7 @@ docker stop $id
 docker rm $id
 rm -rd shopware/html/custom/static-plugins
 mkdir shopware/html/custom/static-plugins
-cp update.sh /shopware/html/update.sh
+cp update.sh shopware/html/update.sh
 git clone --recurse-submodules $PROJECT_REPO_URL shopware/html/custom/static-plugins
 docker compose up -d
 cd shopware/html/custom/static-plugins && git submodule init && git submodule update
